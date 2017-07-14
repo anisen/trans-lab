7811b8b0fb0b17f38a09cd91ac30da938706cff1
“Anais <ananavelez@gmail.com>
2017-07-14 00:09:43 -0400

    css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+
 /* Holiii acá va tu código también */
+
+/*function validacionInputs(){
+	var correo= document.getElementById("correo").value;
+	var contrasena= document.getElementById("contrasena").value;
+	var boton=document.getElementById("boton-iniciosesion");
+
+	boton.addEventListener('click', function(){
+		if (corre!==/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/) {
+			alert("hola")
+		}
+	})
+	
+}*/
+$(document).ready(function() {
+  $(".button-collapse").hide();
+  $(".button-collapse").click(function() {
+    $(".button-collapse").show();
+  });
+  
+  $(".button-collapse").sideNav();
+
+});

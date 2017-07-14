bc2a60e49d1dfdc58ef630924fa9ec3a834adbd0
“Anais <ananavelez@gmail.com>
2017-07-14 12:13:35 -0400

    ocupando api

--- js/main.js
+++ js/main.js
@@ -1,25 +1,49 @@
 
-/* Holiii acá va tu código también */
+/* Holiii acá va tu código también 
 
-/*function validacionInputs(){
-	var correo= document.getElementById("correo").value;
-	var contrasena= document.getElementById("contrasena").value;
-	var boton=document.getElementById("boton-iniciosesion");
+document.getElementById('correo').addEventListener('input', function() {
+	campo = event.target;
+	validado = document.getElementById('emailCorrecto');
 
-	boton.addEventListener('click', function(){
-		if (corre!==/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/) {
-			alert("hola")
-		}
-	})
-	
-}*/
+	emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    if (emailRegex.test(campo.value)) {
+    	validado.innerText = "válido";
+    } else {
+    	validado.innerText = "incorrecto";
+    }
+});*/
+
 $(document).ready(function() {
+	/*sidenav*/
   //$(".button-collapse").hide();
   $(".button-collapse").click(function() {
     //$(".button-collapse").show();
 });
   
   $(".button-collapse").sideNav();
-
+  /*sidnav*/
+  /*menu desplegable*/
   $('.collapsible').collapsible();
+  /*menu desplegable*/
+
+  /*API*/
+  $("#boton-saldo").click(function(i) {
+  	i.preventDefault();
+  	var id= $("#input-saldo").val();
+
+  	$.ajax({
+  		url: 'https://bip-servicio.herokuapp.com/api/v1/solicitudes.json?bip='+id +'',
+  		type: 'GET',
+  		dataType: 'json',
+  	})
+  	.done(function(x) {
+  		console.log(x.saldoTarjeta);
+        $('#vacio').append('<p>'+x.saldoTarjeta+'</p>')
+  	})
+  	.fail(function() {
+  		console.log("error");
+  	});
+  }); 
+  /*API*/ 
 });
